2c5879640cbbc4c834902f470d1e4a0a51654d27
Yuxing Fei <564686325@qq.com>
2021-02-18 22:20:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='material_parser',
     packages=find_packages(),
-    version='6.0.1',
+    version='6.1.2',
     author='Synthesis Project Team',
     author_email='0lgaGkononova@yandex.ru',
     description='Material Synthesis Project',
